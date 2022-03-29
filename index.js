0c36c663129e330e038a01014e0f9f3e6945488d
Sergio Santiago <sergiosantiago0403@gmail.com>
2022-03-29 08:47:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const path = require('path')
+const helmet = require('helmet')
+
+const app = express()
+app.use(express.json()) // parser de requests body como json
+app.use(cors()) // habilita peticiones desde otro dominio
+app.use(helmet()) // seguridad general en servicios REST
+
+
+
+const { initSequelize } = require('./config/sequelize')
+const sequelize = initSequelize()
+
+sequelize.authenticate()
+  .then(() => {
+    console.info('INFO - Database connected.')
+    const port = process.env.APP_PORT || 3600
+    return app.listen(port)
+  })
+  .then((server) => {
+    console.log('Deliverus listening at http://localhost:' + server.address().port)
+  })
+  .catch(err => {
+    console.error('ERROR - Unable to connect to the database:', err)
+  })
